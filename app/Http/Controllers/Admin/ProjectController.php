ad7855f9a22080e936cb677f4301852bd894b5ab
MykolaDekusar <ndekusar97@gmail.com>
2024-07-17 18:00:47 +0200

    [UPD]Modified routes in app

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -6,6 +6,7 @@ use App\Models\Project;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreProjectRequest;
 use App\Http\Requests\UpdateProjectRequest;
+use PhpParser\Node\Stmt\Echo_;
 
 class ProjectController extends Controller
 {
@@ -14,7 +15,6 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        //
     }
 
     /**
